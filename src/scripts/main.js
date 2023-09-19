30eb995c1c4b8917c01c113d1d33ab056a48b1a5
Matias Ludueña <matiasluduena23@gmail.com>
2023-09-19 13:48:55 -0300

    add fetch

---
+++ src/scripts/main.js
@@ -0,0 +1,22 @@
+
+
+
+// async function getAdvice() {
+    
+//     const res = await fetch('https://api.adviceslip.com/advice');
+//     const {slip} = await res.json();
+//     displayAdvice(slip);
+  
+// }
+
+function displayAdvice(data){
+    const idElement = document.querySelector('.id');
+    const adviceElement = document.querySelector('.advice');   
+    idElement.innerHTML = data.id;
+    adviceElement.innerHTML = data.advice; 
+}
+
+
+const btn = document.querySelector('.btn').addEventListener('click', getAdvice )
+
+
